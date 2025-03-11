655778f520db728e4e1dceb9f5b288d265c4960b
17LiQi <yzh859708106@outlook.com>
2025-03-11 19:03:29 +0800

    整合原始项目
    将前后端分离的项目整合到一个文件中,便于管理和拉取

--- README.md
+++
@@ -1,2 +0,0 @@
-# NTAS
-基于mysql数据库,springboot后端,vue前端的全国交通系统模拟项目
--- server/src/main/java/com/yueqi/ntas/serverApplication.java
+++ server/src/main/java/com/yueqi/ntas/serverApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NtasApplication {
+public class serverApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NtasApplication.class, args);
+        SpringApplication.run(serverApplication.class, args);
     }
 
 }
